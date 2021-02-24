60fac5b762e91f5e93c0d057e24a28ebee81972f
Pal <pal@zeamty.com>
2021-02-24 21:01:39 +0800

    update
    Change-Id: Iba944812d150bea068727c280da07a7b1ce6e938

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -6,16 +6,48 @@ module.exports = {
         nav: [
             { text: '主页', link: '/' },
             {
+                text: '基础',
+                items: [
+                    { text: '数据结构与算法', link: '' },
+                    { text: '计算机网络', link: '' },
+                    { text: '操作系统', link: '' },
+                    { text: '计算机组成原理', link: '' },
+                    { text: '数据库原理', link: '' },
+                    { text: '软件工程', link: '' },
+                    { text: '安全', link: '' },
+                ],
+            },
+            {
+                text: '编程语言',
+                items: [
+                    { text: 'Python', link: '' },
+                    { text: 'Go', link: '' },
+                    { text: 'Java', link: '' },
+                    { text: 'JavaScript', link: '' },
+                    { text: '其他', link: '' },
+                ],
+            },
+            {
+                text: '应用',
+                items: [
+                    { text: '', link: '' },
+                    { text: '', link: '' },
+                    { text: '', link: '' },
+                    { text: '', link: '' },
+                    { text: '', link: '' },
+                ],
+            },
+            {
                 text: '其他',
                 items: [
-                    { text: '2018', link: '/Others/2018/05-01' },
+                    { text: '2018', link: '/others/2018-05-01' },
                 ],
             },
             { text: '关于', link: '/about' },
         ],
         sidebar: {
-            '/2018/': [
-                '05-01',
+            '/others/': [
+                '2018-05-01',
             ],
         },
     },
